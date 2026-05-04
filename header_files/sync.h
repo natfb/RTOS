3b3e087fd1bbc780d8f01a0d5e2078f870134682
Natalia Farias <natifari4s@gmail.com>
2026-05-04 16:07:49 -0300

    fiz uma implementacao de mutex

--- header_files/sync.h
+++ header_files/sync.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "os_config.h"
 
-// Padrão POSIX
+// Padrï¿½o POSIX
 typedef struct sem {
     int contador;
     uint8_t fila[MAX_USER_TASKS];
@@ -17,6 +17,24 @@ void sem_init(sem_t *sem, uint8_t valor);
 void sem_wait(sem_t *sem);
 void sem_post(sem_t *sem);
 
+
+// MUTEX
+typedef struct task_queue {
+    uint8_t tasks[MAX_USER_TASKS];
+    int head;
+    int tail;
+    int count;
+} task_queue_t;
+
+typedef struct mutex {
+    uint8_t        value;                       // 0 = free; 1 = not free
+    task_queue_t   wait_list;
+    uint8_t        owner_task_pos;
+} mutex_t;
+
+void mutex_init(mutex_t *mutex);
+void mutex_lock(mutex_t *mutex);
+void mutex_unlock(mutex_t *mutex);
 
 #endif	/* SYNC_H */
 
--- sync.c
+++ sync.c
@@ -1,6 +1,7 @@
 #include "./header_files/sync.h"
 #include "./header_files/kernel.h"
 #include "./header_files/scheduler.h"
+#include "header_files/os_config.h"
 
 // Fila de aptos
 extern ready_queue_t r_queue;
@@ -43,3 +44,69 @@ void sem_post(sem_t *sem)
     
     ENABLE_ALL_INTERRUPTS();    
 }
+
+// MUTEX
+
+void mutex_init(mutex_t *mutex) {
+    mutex->value = 0;
+    mutex->owner_task_pos = -1;
+    mutex->wait_list.count = 0;
+    mutex->wait_list.head = 0;
+    mutex->wait_list.tail = 0;
+    
+    for (int i = 0; i < MAX_USER_TASKS; i++) {
+        mutex->wait_list.tasks[i] = 0;
+    }
+}
+
+void mutex_lock(mutex_t *mutex) {
+    DISABLE_ALL_INTERRUPTS();
+
+    if (mutex->value == 0) {
+        mutex->value = 1;
+        mutex->owner_task_pos = r_queue.pos_task_running;
+    } else {
+        mutex->wait_list.tasks[mutex->wait_list.head] =  r_queue.pos_task_running;
+        mutex->wait_list.head = (mutex->wait_list.head + 1) % MAX_USER_TASKS;
+        mutex->wait_list.count++;
+
+        r_queue.TASKS[r_queue.pos_task_running].task_state = WAITING;
+        ENABLE_ALL_INTERRUPTS();
+        os_yield();
+    }
+
+    ENABLE_ALL_INTERRUPTS(); 
+}
+
+void mutex_unlock(mutex_t *mutex) {
+    DISABLE_ALL_INTERRUPTS();
+
+    if (mutex->owner_task_pos != r_queue.pos_task_running) {
+        ENABLE_ALL_INTERRUPTS(); 
+        return;
+    }
+
+    if (mutex->wait_list.count == 0) {
+        mutex->value = 0;
+        mutex->owner_task_pos = -1;
+    } else {
+        uint8_t next_task = mutex->wait_list.tasks[mutex->wait_list.tail];
+        mutex->wait_list.tail = (mutex->wait_list.tail + 1) % MAX_USER_TASKS;
+        mutex->wait_list.count--;
+
+        mutex->owner_task_pos = next_task;
+        
+        r_queue.TASKS[next_task].task_state = READY;
+    }
+
+    ENABLE_ALL_INTERRUPTS(); 
+}
+
+// exemple usage
+
+// mutex_t m;
+
+// mutex_init(&m);
+// mutex_lock(&m);
+// // regiao critica
+// mutex_unlock(&m);
