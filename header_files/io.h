71a37602bac9e7be8aedf72ad3a65c38910c47b3
Natalia Farias <natifari4s@gmail.com>
2026-05-04 10:07:50 -0300

    adicionei modulo adc feito pelo professor

--- header_files/io.h
+++ header_files/io.h
@@ -1,6 +1,14 @@
 #ifndef IO_H
 #define	IO_H
 
+#include <xc.h>
+#include <stdint.h>
+
+void adc_config(void);
+void adc_on(void);
+uint16_t adc_read(void);
+
+
 
 #endif	/* IO_H */
 
--- io.c
+++ io.c
@@ -1,1 +1,32 @@
+#include "./header_files/io.h"
+
+void adc_config(void)
+{
+    ADCON0bits.CHS      = 0b00000;      // Canal zero (AN0)
+    ADCON1bits.PVCFG    = 0b00;         // VDD
+    ADCON1bits.NVCFG    = 0b00;         // VSS
+    ADCON2bits.ADFM     = 1;            // Justifica��o do dado (direita)
+    ADCON2bits.ACQT     = 0b110;        // Tempo de aquisi��o do sinal
+    ADCON2bits.ADCS     = 0b100;        // Fosc/4
+    TRISAbits.RA0       = 1;
+    ANSELAbits.ANSA0    = 1;
+}
+
+void adc_on(void)
+{
+    ADCON0bits.ADON = 1;
+}
+
+uint16_t adc_read(void)
+{
+    uint16_t dados_adc;
+    
+    ADCON0bits.GO = 1;  // Inicia a amostragem do sinal anal�gico
+    
+    while (ADCON0bits.GODONE);
+    
+    dados_adc = ADRES;
+    
+    return dados_adc;
+}
 
