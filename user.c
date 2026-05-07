cda4edb48d36f4faaaeb5fdce7fe019553a5850b
Natalia Farias <natifari4s@gmail.com>
2026-05-07 18:14:04 -0300

    valor mocado do sensor

--- user.c
+++ user.c
@@ -18,6 +18,7 @@ void config_user()
     ANSELDbits.ANSD0    = 0;
     ANSELCbits.ANSC6    = 0;
     ANSELCbits.ANSC7    = 0;
+    TRISDbits.RD7       = 0;
     
     asm("global _read_sensor_data, _sensor_data_processor");
 
@@ -30,44 +31,27 @@ void config_user()
 
 TASK read_sensor_data() {
     // codigo og
-    // uint16_t sensor_data = 0;
-
-    // while(1) {
-    //     sensor_data = adc_read();
-    //     pipe_write(&p, (char)sensor_data);
-    // }
-
-    // teste sem pipe
     uint16_t sensor_data = 0;
-    float test_temp = 0;
 
     while(1) {
-        // zsensor_data = adc_read(); 
-
-        test_temp = 45;//(sensor_data * 5.0 / 1023.0) * 100.0;
-
-        if (test_temp > 30.0) {
-            PORTDbits.RD7 = 1;  
-        } else {
-            PORTDbits.RD7 = 0;  
-        }
-
-        os_delay(200); 
+        // sensor_data = adc_read();
+        // 61V = 30ºC, ou seja >61 acende led <61 apaga
+        sensor_data = 60;
+        pipe_write(&p, (char)sensor_data);
     }
 }
 
 TASK sensor_data_processor() {
-    // uint16_t sensor_data = 0;
+    uint16_t sensor_data = 0;
     while(1) {
-        // pipe_read(&p, (char *)&sensor_data);
+        pipe_read(&p, (char *)&sensor_data);
 
-        // CURRENT_TEMPERATURE = (sensor_data * 5.0 / 1023.0) * 100.0;
+        CURRENT_TEMPERATURE = (sensor_data * 5.0 / 1023.0) * 100.0;
 
-        // if (CURRENT_TEMPERATURE > 30) {
-        //     sem_post(&s);
-        //     PORTDbits.RD7 = 1;
-        // }
+        if (CURRENT_TEMPERATURE > 30) {
+            sem_post(&s);
+            PORTDbits.RD7 = 1;
+        }
         os_delay(100);
     }
-
 }
